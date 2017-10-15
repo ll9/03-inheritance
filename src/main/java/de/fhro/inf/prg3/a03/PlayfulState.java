e694f0bcce190a7aad5d7af095a45da3336d2026
ll9 <llwierer@web.de>
2017-10-15 14:12:30 +0200

    fixed bug in PlayfulState

--- src/main/java/de/fhro/inf/prg3/a03/PlayfulState.java
+++ src/main/java/de/fhro/inf/prg3/a03/PlayfulState.java
@@ -2,7 +2,7 @@ package de.fhro.inf.prg3.a03;
 
 public class PlayfulState extends State {
     public PlayfulState(Animal animal) {
-        super(animal, animal.getAwake(), "playful");
+        super(animal, animal.getAwake()-animal.getDigest(), "playful");
     }
 
     @Override
