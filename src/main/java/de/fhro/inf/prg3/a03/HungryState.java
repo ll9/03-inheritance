11f01b4fa1a117f3b3241ca9800d7fa7d381e96f
ll9 <llwierer@web.de>
2017-10-15 13:13:49 +0200

    implemented hungryState

---
+++ src/main/java/de/fhro/inf/prg3/a03/HungryState.java
@@ -0,0 +1,18 @@
+package de.fhro.inf.prg3.a03;
+
+public class HungryState extends State {
+    public HungryState(Animal animal) {
+        super(animal, animal.getAwake());
+    }
+
+    @Override
+    State successor() {
+        logger.info("I've starved for a too long time...good bye...");
+        return new DeathState(animal);
+    }
+
+    State feed() {
+        logger.info("You feed the cat...");
+        return new DigestingState(animal);
+    }
+}
