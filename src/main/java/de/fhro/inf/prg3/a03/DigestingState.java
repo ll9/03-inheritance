31b6d806c57b76b71fcc5a8cbd21adc776a369d2
ll9 <llwierer@web.de>
2017-10-15 13:19:13 +0200

    implemented digestingState

---
+++ src/main/java/de/fhro/inf/prg3/a03/DigestingState.java
@@ -0,0 +1,13 @@
+package de.fhro.inf.prg3.a03;
+
+public class DigestingState extends State {
+    public DigestingState(Animal animal) {
+        super(animal, animal.getDigest());
+    }
+
+    @Override
+    State successor() {
+        logger.info("Getting in a playful mood!");
+        return new PlayfulState(animal);
+    }
+}
